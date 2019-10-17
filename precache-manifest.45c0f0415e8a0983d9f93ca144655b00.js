5550c44a76e60420e4e9f07347b905844b8e8a74
Paris Cornett <pariscornett@gmail.com>
2019-10-16 22:09:17 -0500

    Updates

---
+++ precache-manifest.45c0f0415e8a0983d9f93ca144655b00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f04599fe97b522601bd521b2cd1f2c5",
+    "url": "/Professional-Portfolio/index.html"
+  },
+  {
+    "revision": "e54ef489b87858c61b77",
+    "url": "/Professional-Portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "b8f1528825aa15ec7f0d",
+    "url": "/Professional-Portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e54ef489b87858c61b77",
+    "url": "/Professional-Portfolio/static/js/2.56630230.chunk.js"
+  },
+  {
+    "revision": "b8f1528825aa15ec7f0d",
+    "url": "/Professional-Portfolio/static/js/main.5418a142.chunk.js"
+  },
+  {
+    "revision": "ab8ff4aab5dac2a82e42",
+    "url": "/Professional-Portfolio/static/js/runtime-main.efb1d683.js"
+  },
+  {
+    "revision": "9fbe5ec4f2162974795b8e9d87e9e72c",
+    "url": "/Professional-Portfolio/static/media/myAvatar.9fbe5ec4.png"
+  }
+]);
